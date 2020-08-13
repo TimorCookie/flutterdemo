899aa363bb09c38d1de703e47af4fa7a31a8bf89
timokie <timokie@jiliguala.com>
2020-08-13 17:24:39 +0800

    remove courses

--- lib/pages/tabs/Category.dart
+++ lib/pages/tabs/Category.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
-import '../../res/courses.dart';
 
 class CategoryTab extends StatelessWidget {
-  const CategoryTab({Key key}) : super(key: key);
+  List courses = [
+    {"type": '儿歌', "img": "https://qiniucdn.jiliguala.com/L1XX_L6XX_img3.png"},
+    {
+      "type": '单词',
+      "img": "https://qiniucdn.jiliguala.com/B2MC_L0XX_L6XX_img3.png"
+    },
+    {
+      "type": '绘本',
+      "img": "https://qiniucdn.jiliguala.com/L0XX_L6XX_Entity_0_6_img3.png"
+    },
+    {
+      "type": '口语',
+      "img": "https://qiniucdn.jiliguala.com/L1XX_L2XX_img.png",
+    },
+    {
+      "type": '动画',
+      "img": "https://qiniucdn.jiliguala.com/L1XX_L6XX_Entity_1_6_img3.png",
+    }
+  ];
+
+  CategoryTab({Key key}) : super(key: key);
 
   Widget _getCourse(context, index) {
     return Card(
@@ -22,7 +41,7 @@ class CategoryTab extends StatelessWidget {
               ListTile(
                 // leading: Image.network(courses[index]['img']),
                 title: Text(
-                  courses[index]['type'],
+                  this.courses[index]['type'],
                   style: TextStyle(
                     color: Colors.green,
                     fontWeight: FontWeight.bold,
